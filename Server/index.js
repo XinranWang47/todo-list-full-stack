addc1cacd6f4b2b56c4531cedeb5a0a306843214
xrhywww <admin@Admins-MacBook-Air-2.local>
2025-05-08 17:51:47 +0100

    create .env and revise index.js

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,6 @@
 
+require('dotenv').config(); 
+
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
@@ -9,7 +11,12 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('✅ MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/get',(req,res) => {
   TodoModel.find()
